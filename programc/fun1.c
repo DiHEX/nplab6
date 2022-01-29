598d8e920eb3de74cce97fde39840dbc2754c34d
DiHEX <kaligramxd@gmail.com>
2022-01-29 19:31:10 +0100

    Dodaje fun1 go gałęzi 1

--- programc/fun1.c
+++ programc/fun1.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //
 // Created by tomas on 29.01.2022.
 //
@@ -6,3 +7,13 @@
 void fun1(){
     printf("Fun1.c dziala\n");
 }
+=======
+//
+// Created by tomas on 29.01.2022.
+//
+#include <stdio.h>
+#include "fun1.h"
+void fun1(){
+    printf("Fun1.c dziala\n");
+}
+>>>>>>> Dodaje fun1 go gałęzi 1
